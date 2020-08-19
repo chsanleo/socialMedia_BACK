640a975117abd80f733194d01cd2e1fda1bef07b
Christian <chsanleo@gmail.com>
2020-08-19 12:58:34 +0200

    addMethods
    findById + update user

--- SRC/core/repositories/userRepository.js
+++ SRC/core/repositories/userRepository.js
@@ -5,16 +5,29 @@ const UserRepository = {
     async searchByEmail(email) {
         try {
             return await User.findOne({ where: { email: email } });
-        } catch (error) { 
+        } catch (error) {
             console.log(error);
             return null;
         }
     },
-
-    async create(user){
+    async create(user) {
         try {
             await User.create(user);
-        } catch (error) { 
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async findById(id) {
+        try {
+            await User.findOne({ where: { id: id, deletedAt: null } });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async update(id, user) {
+        try {
+            await User.update(user, { where: { id: id } });
+        } catch (error) {
             console.log(error);
         }
     }
