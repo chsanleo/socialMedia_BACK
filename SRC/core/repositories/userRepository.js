815ca20eef732e94cc0ed7d9a623a9312525d1d0
Christian <chsanleo@gmail.com>
2020-08-19 14:15:40 +0200

    Create method findbycity users

--- SRC/core/repositories/userRepository.js
+++ SRC/core/repositories/userRepository.js
@@ -2,14 +2,6 @@ const { User } = require('../../models');
 
 const UserRepository = {
 
-    async searchByEmail(email) {
-        try {
-            return await User.findOne({ where: { email: email } });
-        } catch (error) {
-            console.log(error);
-            return null;
-        }
-    },
     async create(user) {
         try {
             await User.create(user);
@@ -22,6 +14,22 @@ const UserRepository = {
             return await User.findByPk(id);
         } catch (error) {
             console.log(error);
+        }
+    },
+    async searchByEmail(email) {
+        try {
+            return await User.findOne({ where: { email: email } });
+        } catch (error) {
+            console.log(error);
+            return null;
+        }
+    },
+    async searchByCity(){
+        try {
+            return await User.where({ city: user.city });
+        } catch (error) {
+            console.log(error);
+            return null;
         }
     },
     async update(id, user) {
