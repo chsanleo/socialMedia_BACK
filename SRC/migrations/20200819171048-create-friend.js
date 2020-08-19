ca4bdcd1d565163769cfa06fea2557b22afa7f96
Christian <chsanleo@gmail.com>
2020-08-19 20:19:29 +0200

    Create migration Friend

---
+++ SRC/migrations/20200819171048-create-friend.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Friends', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      friend1: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      friend2: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdBy: {
+        type: Sequelize.INTEGER
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      deletedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Friends');
+  }
+};
