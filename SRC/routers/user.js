f29b34bbc0d9828735ded0755f7d99ff5140b613
Christian <chsanleo@gmail.com>
2020-08-19 12:49:40 +0200

    route for userController

--- SRC/routers/user.js
+++ SRC/routers/user.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const MainController = require('../core/controllers/userController');
+const UserController = require('../core/controllers/userController');
 
+router.post('/update', UserController.update);
+router.get('/delete', UserController.delete);
+router.get('/usersNear', UserController.usersNear);
+//router.get('/usersJobs', UserController.usersNear);
+//router.get('/usersHobbies', UserController.usersNear);
 
-module.exports = router;
+module.exports = router; 
