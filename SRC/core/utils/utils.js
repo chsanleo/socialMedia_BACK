55a9dd698fc737dbdd53f1e5661243e655178665
Christian <chsanleo@gmail.com>
2020-08-19 11:50:59 +0200

    FIX random string

--- SRC/core/utils/utils.js
+++ SRC/core/utils/utils.js
@@ -8,7 +8,7 @@ const Utils = {
         let universe = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!?()[]{}$&%-+';
         let universeLenght = universe.lenght;
         for(let i = 0; i<lenght; i++){
-            result += universe.chatAt(Math.floor(Math.random()*universeLenght));
+            result += universe[Math.floor(Math.random()*universeLenght)];
         }
         return result;
     }
