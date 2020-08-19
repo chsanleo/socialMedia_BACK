a1a86b4c673f6cc96f97b87395bf0b9ddddf90c0
Christian <chsanleo@gmail.com>
2020-08-19 14:16:22 +0200

    Adapt userController to new pattern

--- SRC/core/controllers/userController.js
+++ SRC/core/controllers/userController.js
@@ -1,82 +1,45 @@
-const Validations = require('../utils/validations');
+const _userService = require('../services/userService');
 
-const MainController = {
+const UserController = {
 
-    async update(req, res) {
-        try {
-//exist
-            const id = parseInt(req.body.id);
-
-            Validations.validaId(id);
-
-            const user = await User.findOne({
-                where: { id: id, deletedAt: null }
+    update(req, res) {
+        const user = {
+            username: req.body.username,
+            name: req.body.name,
+            surname: req.body.surname,
+            pic_path: req.body.pic_path,
+            birthdate: req.body.birthdate,
+            address: req.body.adress,
+            country: req.body.country,
+            city: req.body.city,
+            hobbies: req.body.hobbies,
+            password: '',
+            email: ''
+        };
+        _userService.update(req.body.id, user)
+            .then(res.status(200).send())
+            .catch(error => {
+                console.log(error);
+                res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
             });
-//
-            if (!user) { throw Error(' User error. '); }
-
-            const userF = {
-                username: req.body.username,
-                name: req.body.name,
-                surname: req.body.surname,
-                email: req.body.email,
-                password: user.password,
-                pic_path: req.body.pic_path,
-                birthdate: req.body.answerSecret,
-                address: req.body.adress,
-                country: req.body.country,
-                city: req.body.city
-            };
-
-            Validations.validaUser(userF);
-
-            await User.update(userF, {
-                where: { id: id }
-            });
-            res.status(200).send({ message: 'Todo OK' });
-        } catch (error) {
-            console.log(error);
-            res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
-        }
     },
-    async delete(req, res) {
-        try {
-//exist
-            const id = parseInt(req.body.id);
+    delete(req, res) {
 
-            Validations.validaId(id);
-
-            const user = await User.findOne({
-                where: { id: id, deletedAt: null }
+        _userService.delete(req.body.id)
+            .then(res.status(200).send())
+            .catch(error => {
+                console.log(error);
+                res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
             });
-//
-            if (!user) { throw Error(' User error. '); }
-
-            await User.delete(id);
-            res.status(200);
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
-        }
     },
     async usersNear(req, res) {
-        try {
-//exist
-            let id = req.body.id;
-
-            Validations.validaId(id);
-
-            const user = await User.findOne({
-                where: { id: id, deletedAt: null }
+        _userService.usersNear(req.body.id)
+            .then(userNear => res.status(200).send(userNear))
+            .catch(error => {
+                console.log(error);
+                res.status(500).send({ message: 'There was an error. Contact with the administrator.' });
             });
-//
-            if (!user) { throw Error(' User error. '); }
-
-            const userList = await User.where({ city: user.city});
-            res.status(201).send(userList);
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
-        }
     }
 };
+
+module.exports = UserController;
