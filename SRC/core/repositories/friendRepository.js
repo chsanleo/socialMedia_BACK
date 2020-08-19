ab9807bae67c3926dfb34f853e2d6f927d41ca26
Christian <chsanleo@gmail.com>
2020-08-19 20:20:05 +0200

    Create Friend repository

---
+++ SRC/core/repositories/friendRepository.js
@@ -0,0 +1,28 @@
+const { Friend } = require('../../models');
+
+//0 delete, 1 pending, 2 accepted, 3 deny
+const UserRepository = {
+    async create(friendRelation) {
+        try {
+            await Friend.create(friendRelation);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async getAll(idFriend){
+        try {
+            return await Friend.findAll({where: { $or : [ {friend1:idFriend}, {friend2:idFriend}] }});
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async update(id, type) {
+        try {
+           await Friend.update({type:type},{where:{id:id}});
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+};
+module.exports = UserRepository;
