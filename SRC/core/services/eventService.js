9bd78352e728c9397d0ad5d03a07aaa1a7ce0be1
Christian <chsanleo@gmail.com>
2020-08-20 22:10:55 +0200

    create eventService file

---
+++ SRC/core/services/eventService.js
@@ -0,0 +1,8 @@
+const _eventRepository = require('../repositories/eventRepository');
+const Validations = require('../utils/validations');
+
+const EventService = {
+
+};
+
+module.exports = EventService;
