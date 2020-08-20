5180e3ead7e5012d45da4f3b294e7a3947716eac
Christian <chsanleo@gmail.com>
2020-08-20 21:18:33 +0200

    add login to router

--- SRC/routers/main.js
+++ SRC/routers/main.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const MainController = require('../core/controllers/mainController');
 
 router.post('/register', MainController.register);
+router.post('/login', MainController.login);
 
 module.exports = router;
