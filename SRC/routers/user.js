a9a2ee47bbd16a07079b91703a0049695c2b44ea
Christian <chsanleo@gmail.com>
2020-08-20 21:18:44 +0200

    add logout to router

--- SRC/routers/user.js
+++ SRC/routers/user.js
@@ -3,7 +3,8 @@ const UserController = require('../core/controllers/userController');
 
 router.post('/update', UserController.update);
 router.get('/delete', UserController.delete);
-router.get('/user', UserController.getUser);   
+router.get('/user', UserController.getUser);
+router.post('/logout', UserController.logOut);
 router.get('/usersNear', UserController.usersNear);
 //router.get('/usersJobs', UserController.usersNear);
 //router.get('/usersHobbies', UserController.usersNear);
