0ca24627823893c30ecf7693548de1e089d2a9f2
Christian <chsanleo@gmail.com>
2020-08-21 00:29:46 +0200

    Create eventModel (MongoDB)

---
+++ SRC/models/event.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const EventSchema = mongoose.Schema({
+    owner: {
+        //[username,userid]
+        type: Array, required: true
+    },
+    title: {
+        type: String, required: true
+    },
+    body: {
+        type: String, required: true
+    },
+    pic_path: {
+        type: String, required: true
+    },
+    userLikes: {
+        //[idUsers,idUsers,...]
+        type: Array, required: false
+    },
+    userJoin: {
+        //[idUsers,idUsers,...]
+        type: Array, required: false
+    },
+    delete: {
+        type: Boolean, required: true
+    }
+});
+
+const eventModel = mongoose.model('Event', EventSchema);
+module.exports = eventModel;
