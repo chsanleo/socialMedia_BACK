d70457c4520521b11865ba625add6173574cf644
Christian <chsanleo@gmail.com>
2020-08-22 13:25:08 +0200

    event repository methods

--- SRC/core/repositories/eventRepository.js
+++ SRC/core/repositories/eventRepository.js
@@ -1,5 +1,35 @@
+const Event = require('../../models/event');
 
 const EventRepository = {
-   
+    async create(event) {
+        try {
+            event.CreateAt = Date.now();
+            await Event.insert(event);
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async findById(_id) {
+        try {
+            return await Event.find({ _id: _id, delete: false });
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async findAllByType(type) {
+        try {
+            return await Event.find({ type: type, delete: false }).sort('-CreatedAt');
+        } catch (error) {
+            console.log(error);
+        }
+    },
+    async update(event) {
+        try {
+            event.UpdateAt = Date.now();
+            await Event.updateOne({ '_id': event.id }, { upsert: true });
+        } catch (error) {
+            console.log(error);
+        }
+    }
 };
 module.exports = EventRepository;
