2a380cdb8869eaf4a3a6314018e9aeea2b153ece
Christian <chsanleo@gmail.com>
2020-08-22 12:48:31 +0200

    Create a new unic value push in array

--- SRC/core/utils/utils.js
+++ SRC/core/utils/utils.js
@@ -13,6 +13,10 @@ const Utils = {
             result += universeArray[Math.floor((Math.random() * universeLength) + 1)];
         }
         return result;
+    },
+
+    pushUnic(items, item){
+        if(this.items.indexOf(item) === -1) { return this.items.push(item); }
     }
 };
 module.exports = Utils;
