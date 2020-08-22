079cfee885b0410732820c38968cab4ca1853d2e
Christian <chsanleo@gmail.com>
2020-08-22 12:54:50 +0200

    Create route methods to event

--- SRC/routers/event.js
+++ SRC/routers/event.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const EventController = require('../core/controllers/eventController');
 
+router.post('/create',EventController.create);
+router.post('/event',EventController.get);
+router.post('/events',EventController.getAll);
+router.post('/update',EventController.update);
+router.post('/joinEvent',EventController.joinEvent);
+router.post('/likeEvent',EventController.likeEvent);
+router.post('/delete',EventController.delete);
 
 
 module.exports = router; 
