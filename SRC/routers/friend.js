af043e68037772925e119f656fdfb609d8ef26e9
Christian <chsanleo@gmail.com>
2020-08-22 12:51:56 +0200

    change get by post

--- SRC/routers/friend.js
+++ SRC/routers/friend.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const FriendController = require('../core/controllers/friendController');
 
 router.post('/friendrequest', FriendController.createFriendRelation);
-router.get('/allfriends', FriendController.getAllFriendRelations);
+router.post('/allfriends', FriendController.getAllFriendRelations);
 router.post('/denyRequest', FriendController.denyFriendRequest);   
 router.post('/acceptRequesst', FriendController.acceptFriendRequest);
 
