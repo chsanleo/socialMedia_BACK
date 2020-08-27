18793c802f017f726a983f50be5e88b8be9ab1ef
Christian <chsanleo@gmail.com>
2020-08-27 16:07:17 +0200

    add friends resum to user

--- SRC/core/modelsReturn/conversionToReturn.js
+++ SRC/core/modelsReturn/conversionToReturn.js
@@ -1,8 +1,29 @@
+const _friendService = require('../services/friendService');
 
 const ConversionObjects = {
-    userToUserReturn (user){
+    userToUserReturn(user) {
+        //0 delete, 1 pending, 2 accepted, 3 deny
+
+        let friends = 0;
+        let pendingFriends = 0;
+
+        let friendsList = _friendService.getAllFriendRelations(user.id)
+            .then(friendsList => {
+                for (let friend of friendsList) {
+                    if (friend.type === 1 && friend.createdBy !== user.id) {
+                        pendingFriends++;
+                        continue;
+                    }
+                    if (friend.type === 2) {
+                        friends++;
+                        continue;
+                    }
+                }
+            })
+            .catch();
+
         const userReturn = {
-            id : user.id,
+            id: user.id,
             username: user.username,
             name: user.name,
             surname: user.surname,
@@ -10,10 +31,12 @@ const ConversionObjects = {
             pic_path: user.pic_path,
             birthdate: user.birthdate,
             address: user.address,
-            CountryId : user.CountryId,
+            CountryId: user.CountryId,
             city: user.city,
             hobbies: user.hobbies,
-            job: user.job
+            job: user.job,
+            numFriends: friends,
+            pendingFriends: pendingFriends
         }
         return userReturn;
     }
