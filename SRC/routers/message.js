1a2c0537de8359db0be98f3809fe63a3d9826250
Christian <chsanleo@gmail.com>
2020-09-01 02:38:48 +0200

    Fix, rename to message

--- SRC/routers/message.js
+++ SRC/routers/message.js
@@ -1,13 +1,12 @@
 const router = require('express').Router();
 const MessageController = require('../core/controllers/messageController');
 
-router.post('/create',MessageController.create);
-router.post('/event',MessageController.get);
-router.post('/events',MessageController.getAll);
-router.post('/update',MessageController.update);
-router.post('/likeEvent',MessageController.likeMessage);
-router.post('/unlikeEvent',MessageController.unLikeMessage);
-router.post('/delete',MessageController.delete);
-
+router.post('/create', MessageController.create);
+router.post('/message', MessageController.get);
+router.post('/messages', MessageController.getAll);
+router.post('/update', MessageController.update);
+router.post('/likeMessage', MessageController.likeMessage);
+router.post('/dislikeMessage', MessageController.unLikeMessage);
+router.post('/delete', MessageController.delete);
 
 module.exports = router; 
